7d370641bf4c867976cbeacc756d534d95bca410
benfowlfx <ben.fowl@currencycloud.com>
2016-07-11 15:56:27 +0100

    Favourite model created to pass the spec tests

---
+++ app/models/favourite.rb
@@ -0,0 +1,5 @@
+class Favourite < ApplicationRecord
+  validates_presence_of :number
+  validates_uniqueness_of :number
+  validates_numericality_of :number, greater_than: 0, less_than_or_equal_to: 100_000_000_000
+end
