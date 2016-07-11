b817f610d6b42d133c7b270db4f0d1aa6230e6e0
benfowlfx <ben.fowl@currencycloud.com>
2016-07-11 17:13:10 +0100

    Failing cucumber spec to meet first criteria, need to be able to paginate FizzBuzz items, I don't want to persist all of them though

--- spec/controllers/api/v1/fizz_buzzs_controller_spec.rb
+++ spec/controllers/api/v1/fizz_buzzs_controller_spec.rb
@@ -7,5 +7,13 @@ RSpec.describe Api::V1::FizzBuzzsController, type: :controller do
       expect(response.status).to eq 200
       expect(response.content_type).to eq('application/json')
     end
+
+    context 'No query params' do
+      it 'should return a list of 100 FizzBuzz items and should pass schema validation' do
+        get :index
+        expect(response_body['data'].count).to eq 100
+        expect(response).to match_response_schema('fizz_buzz')
+      end
+    end
   end
 end
