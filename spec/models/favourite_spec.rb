65b1d76949321d01db4297fa581779bc88ae18a8
benfowlfx <ben.fowl@currencycloud.com>
2016-07-11 16:12:19 +0100

    Fixed spec after rubocop update

--- spec/models/favourite_spec.rb
+++ spec/models/favourite_spec.rb
@@ -42,7 +42,7 @@ RSpec.describe Favourite, type: :model do
           is greater than the limit 100000000000' do
         favourite = Favourite.create(number: 200_000_000_000)
         expect(favourite.valid?).to be_falsey
-        expect(favourite.errors[:number][0]).to eq 'must be less than or equal to 100_000_000_000'
+        expect(favourite.errors[:number][0]).to eq 'must be less than or equal to 100000000000'
       end
 
       it 'should allow a new favourite to be created for the limit number' do
